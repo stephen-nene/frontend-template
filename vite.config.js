6c7fae52053f8b7ba62ddba149c69f97c53fa062
Stephen Nene <stephen.nene@student.moringaschool.com>
2025-03-24 20:13:16 +0300

    add Tauri setup with initial configuration and icons

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname,'./src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-  }
-})
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173, 
+  },
+});
